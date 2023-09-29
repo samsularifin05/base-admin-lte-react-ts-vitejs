955137c8bfb551cf2b6920dd7ca7567675fe5c79
samsularifin05 <samsul026@gmail.com>
2023-09-29 21:35:21 +0700

    add profile

--- src/components/molekul/header/index.tsx
+++ src/components/molekul/header/index.tsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import { Profile } from "@/assets";
+// import { Link } from "react-router-dom";
+// import { useState } from "react";
+// import { Profile } from "@/assets";
 import { AppDispatch, themesActions, useAppSelector } from "@/reduxStore";
 import { useDispatch } from "react-redux";
+import { Navbar, Nav, NavDropdown, Dropdown } from "react-bootstrap";
+// import { Logo } from "@/assets";
 
 const Header = () => {
-  const [menu, setMenu] = useState(false);
+  // const [menu, setMenu] = useState(false);
   const theme = useAppSelector((state) => state.theme);
   const dispatch = useDispatch<AppDispatch>();
 
@@ -13,8 +15,49 @@ const Header = () => {
     dispatch(themesActions.setSidebarToggle(!theme.getSidebarToggle));
   };
   return (
-    <nav className="main-header navbar navbar-expand navbar-white navbar-light">
-      <ul className="navbar-nav">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="light"
+      variant="light"
+      className="main-header navbar navbar-expand navbar-white navbar-light"
+    >
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto navbar-nav">
+          <Nav.Link href="#" className="nav-item">
+            <span
+              onClick={handleToggleMenuSidebar}
+              data-widget="pushmenu"
+              aria-label="Menu Hide Bar"
+              role="button"
+            >
+              <i className="fas fa-bars" />
+            </span>
+          </Nav.Link>
+        </Nav>
+        <Nav>
+          <NavDropdown
+            title={
+              <img
+                src={"https://adminlte.io/themes/v3/dist/img/user2-160x160.jpg"}
+                alt="User Image"
+                className="img-size-50 img-circle"
+              />
+            }
+            id="collasible-nav-dropdown"
+            className="mr-3"
+          >
+            <Dropdown.Item href="#">Ubah Password</Dropdown.Item>
+            <Dropdown.Item href="#">Logout</Dropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+  // <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+  {
+    /* <ul className="navbar-nav">
         <li className="nav-item">
           <span
             className="nav-link"
@@ -26,37 +69,10 @@ const Header = () => {
             <i className="fas fa-bars" />
           </span>
         </li>
-      </ul>
+      </ul> */
+  }
 
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <div className="nav-item dropdown">
-            <Link to="#" className="nav-link dropdown-toggle user-action">
-              <img
-                src={Profile}
-                className="avatarProfile"
-                alt="Avatar"
-                height="64"
-              />{" "}
-              <span className="text-dark">SAM</span> <b className="caret"></b>
-            </Link>
-            <div
-              className={`dropdown-menu ${menu ? "show" : ""}`}
-              onMouseLeave={() => setMenu(false)}
-            >
-              <button type="button" className="dropdown-item">
-                <i className="fa fa-user"></i> Profile
-              </button>
-              <button type="button" className="dropdown-item">
-                <i className="nav-icon fas fa-arrow-right-from-bracket"></i>{" "}
-                Logout
-              </button>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </nav>
-  );
+  // </nav>
 };
 
 export default Header;
